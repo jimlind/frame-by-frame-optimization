3987f671172143b7487b13100bd8c6b16214cb8f
Jim Lind <spoon.vw@gmail.com>
2018-08-11 07:29:19 -0400

    Add Two Helpers
    Stuff that doesn't make sense anywhere else. Now I've got a junk drawer.

--- lib/ImageHelper.php
+++ lib/ImageHelper.php
@@ -1,5 +1,8 @@
 <?php
 class ImageHelper {
+    // 1/2 brightness according to method here
+    const GRAY = 384;
+
     // Not actually color brightness but more white is a lower value than more black
     public static function getColorBrightness($imageResource, int $x, int $y): int {
         $rgb = @imagecolorat($imageResource, $x, $y);
--- lib/MathHelper.php
+++ lib/MathHelper.php
@@ -1,6 +1,15 @@
 <?php
 class MathHelper {
     public static function avg($list) {
+        if (count($list) === 0) {
+            return 0;
+        }
+
         return array_sum($list) / count($list);
+    }
+
+    public static function in($value, $min, $max){
+        // print_r([$value, $min, $max, $min < $value && $value < $max]);
+        return ($min < $value && $value < $max);
     }
 }
