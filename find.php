722eee91d3e8766e8c8fa25de39a10103e6dc030
Jim Lind <spoon.vw@gmail.com>
2018-09-16 13:33:05 -0400

    Rename Script to Find
    Because I guess I've got 2 scripts now. If I come up with a way that combines them I'll probably call that script again.. maybe.

---
+++ find.php
@@ -0,0 +1,35 @@
+<?php
+// HOW TO USE
+// php find.php ../RED-SANDSK/
+// >>>> ../RED-SANDSK/  (this is the path that the `cap` dir is in)
+
+// Setup Simple Class Autoloading
+spl_autoload_register(function ($class_name) {
+    include './lib/' . $class_name . '.php';
+});
+
+// Check CLI argument or give user a prompt
+$inputArg = $argv[1] ?? '';
+if (empty($inputArg)) {
+    print('Enter fully qualified path that has the cap directory: ');
+    $handle = fopen('php://stdin', 'r');
+    $inputArg = fgets($handle);
+}
+
+// Validate input directory
+$inputPath = realpath($inputArg);
+if (!is_dir($inputPath . '/cap')) {
+    throw new Exception('NEED VALID INPUT DIRECTORY');
+}
+
+// Find all neccessary image folders and loop over them
+$allResults = [];
+foreach (glob($inputPath . '/cap/*') as $imageFolder) {
+    $result = Frame::look($imageFolder, '/c*.jpeg');
+    if (!empty($result['top'])) {
+        $allResults = array_merge_recursive($allResults, $result);
+    }
+}
+
+print_r($allResults);
+exit();
