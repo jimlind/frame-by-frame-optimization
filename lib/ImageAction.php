b27afc7f90ac37da1220c3673c8048f822afb909
Jim Lind <spoon.vw@gmail.com>
2018-09-22 12:07:48 -0400

    Give the Image Action some Guts
    Let it do stuff.

--- lib/ImageAction.php
+++ lib/ImageAction.php
@@ -8,6 +8,58 @@ class ImageAction {
     }
 
     public function run() {
-        print('.');
+        $cacheKey = $this->fixDistort($this->filePath);
+        $tmpFile = $this->writePositioningImage($cacheKey);
+        
+        $dataModel = new \Models\ImageDataModel($tmpFile);
+        $centerColumn = $dataModel->getCenterColumn();
+        $leftColumn = $dataModel->getLeftColumn();
+        $rightColumn = $dataModel->getRightColumn();
+    }
+
+    protected function fixDistort(string $imageFile) : string {
+        // Setup the points to use for distortion writing
+        $perspectivePoints = [
+            '560,610 560,610',
+            '560,1650 560,1650',
+            '1960,1635 1960,1650',
+            '1960,610 1960,610',
+        ];
+        
+        // Fix distorition and write to cache
+        $cacheKey = 'mpc:tmp';
+        $fixDistortCommand = [
+            'convert',
+            $imageFile,
+            '-distort Barrel "0.0 -0.03 0.0 1.03"',
+            '-write mpr:distort',
+            '+delete',
+            '\( mpr:distort',
+            '-distort Perspective "'.implode(' ', $perspectivePoints).'"',
+            '+write',
+            $cacheKey,
+            '\) null:',
+        ];
+        shell_exec(implode(' ', $fixDistortCommand));
+
+        return $cacheKey;
+    }
+
+    protected function writePositioningImage(string $cacheKey, string $tmpFile = '') : string{
+        // If nothing was supplied.. use a tmp file
+        if (empty($tmpFile)) {
+            $tmpFile = sys_get_temp_dir() . '/film-sprocket-hole.jpg';
+        }
+
+        // Perform conversion
+        $cmd = [
+            'convert',
+            $cacheKey,
+            '-quality 92',
+            $tmpFile
+        ];
+        shell_exec(implode(' ', $cmd));
+
+        return $tmpFile;
     }
 }
