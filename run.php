2ec0c70cf22f0d61a8534597473122d89446df6f
Jim Lind <spoon.vw@gmail.com>
2018-09-22 12:08:33 -0400

    Move to New Autoloader.
    Name Things Better
    Use ImageFolderAction

--- run.php
+++ run.php
@@ -1,15 +1,12 @@
 <?php
+include_once './lib/Autoloader.php';
+
 // HOW TO USE
 // php run.php ../RED-SANDSK/ 'output-test' '003/c009*' 1
 // >>>> ../RED-SANDSK/  (this is the path that the `cap` dir is in)
 // >>>> 'output-test'   (this is the output dir we are creating next to the `cap`; defaults to 'output')
 // >>>> '003/c009*'     (this is the filtering we are putting on input images; defaults to '*/c*.jpeg')
 // >>>> 1               (this is to keep the positioning image for debugging purposes)
-
-// Setup Simple Class Autoloading
-spl_autoload_register(function ($class_name) {
-    include './lib/' . $class_name . '.php';
-});
 
 // Check CLI argument or give user a prompt
 $inputArg = $argv[1] ?? '';
@@ -21,28 +18,34 @@ if (empty($inputArg)) {
 
 // Validate input directory
 $inputPath = realpath($inputArg);
-if (!is_dir($inputPath . '/cap')) {
+$capturePath = $inputPath . '/cap';
+if (!is_dir($capturePath)) {
     throw new Exception('NEED VALID INPUT DIRECTORY');
 }
 
 // Check CLI argument for a custom output path
-$outputArg = $argv[2] ?? 'output';
-$outputPath = $inputPath . '/' . $outputArg;
-$outputPath = FileSystemHelper::make($outputPath);
+$outputDir = $argv[2] ?? 'output';
+$outputPath = $inputPath . '/' . $outputDir;
 
 // Check CLI argument for file limiters
 $limitArgString = $argv[3] ?? '';
 $limitArgList = array_filter(explode('/', $limitArgString));
-$capPathGlobInput = $inputPath . '/cap/' . ($limitArgList[0] ?? '*');
+$capPathGlobInput = $capturePath . '/' . ($limitArgList[0] ?? '*');
 $fileGlobInput = '/' . ($limitArgList[1] ?? 'c*') . '.jpeg';
 
 // Check CLI argument for positing image retention
 $keepPositioningImage = (bool) ($argv[4] ?? false);
 
 // Find all neccessary image folders and loop over them
-foreach (glob($capPathGlobInput) as $imageFolder) {
-    Frame::convert($imageFolder, $fileGlobInput, $outputPath, $keepPositioningImage);
+foreach (glob($capPathGlobInput) as $imageInputPath) {
+    $imageOutputPath = $outputPath . '/' . basename($imageInputPath);
+
+    $imageFolderAction = new ImageFolderAction($imageInputPath, $imageOutputPath);
+    $imageFolderAction->fileGlobInput = $fileGlobInput;
+    $imageFolderAction->keepPositioningImage = $keepPositioningImage;
+    $imageFolderAction->run();
 }
+
 // TODO: When I know this is working perfectly renable video conversion
 // Video::convert($outputPath);
 exit();
