ab7ac1bd65d25ef6819103e4a7dd97238e9a82d0
Jim Lind <spoon.vw@gmail.com>
2018-09-22 23:35:43 -0400

    Add Sproket Column to Data Model

--- lib/Models/ImageDataModel.php
+++ lib/Models/ImageDataModel.php
@@ -5,6 +5,7 @@ use \Helpers\ImageHelper;
 
 class ImageDataModel {
     // This is basically the X of a left-ish and right-ish parts of the frame are.
+    const X_SPROKET_VALUE = 400;
     const X_LEFT_VALUE = 700;
     const X_RIGHT_VALUE = 1800;
 
@@ -15,6 +16,8 @@ class ImageDataModel {
     protected $leftColumn = [];
 
     protected $rightColumn = [];
+
+    protected $sproketColumn = [];
     
     public function __construct(string $imageFilePath) {
         $this->resource = imagecreatefromjpeg($imageFilePath);
@@ -45,5 +48,11 @@ class ImageDataModel {
         return $this->rightColumn;
     }
 
+    public function getSproketColumn(): array {
+        if (empty($this->sproketColumn)) {
+            $this->sproketColumn = ImageHelper::gatherColorBrightnessList($this->resource, self::X_SPROKET_VALUE);
+        }
 
+        return $this->sproketColumn;
+    }
 }
