e13fffee7341c44a17b27d7130f56b3b072dc5f4
Jim Lind <spoon.vw@gmail.com>
2018-09-25 20:45:14 -0400

    More Advanced Color Support
    Use an actual algorithm to get luminosity and check the 9-pixel square to get an average.

--- lib/Helpers/ImageHelper.php
+++ lib/Helpers/ImageHelper.php
@@ -10,24 +10,43 @@ class ImageHelper {
         $height = imagesy($imageResource);
         $brightnessList = [];
         for ($y = 0; $y <= $height; $y++) {
-            $brightnessList[$y] = self::getColorBrightness($imageResource, $x, $y);
+            $brightnessList[$y] = self::getSpotColorBrightness($imageResource, $x, $y);
         }
 
         return $brightnessList;
     }
 
     /**
+     * Get a value representing the nine pixels surrounding a single spot
+     * Not actually color brightness but more white is a lower value than more black
+     */
+    protected static function getSpotColorBrightness($imageResource, int $x, int $y): float {
+        $valueList = [];
+        foreach (range($x-1, $x+1) as $a) {
+            foreach (range($y-1, $y+1) as $b) {
+                $value = self::getColorBrightness($imageResource, $a, $b);
+                if ($value === -1.00) {
+                    continue;
+                }
+                $valueList[] = $value;
+            }
+        }
+
+        return MathHelper::average($valueList);
+    }
+
+    /**
      * Get a value representing a pixel
      * Not actually color brightness but more white is a lower value than more black
      */
-    protected static function getColorBrightness($imageResource, int $x, int $y): int {
+    protected static function getColorBrightness($imageResource, int $x, int $y): float {
         $rgb = @imagecolorat($imageResource, $x, $y);
         if($rgb === false) {
             return -1;
         }
         $colors = imagecolorsforindex($imageResource, $rgb);
-        $average = (256 * 3) - $colors['red'] - $colors['green'] - $colors['blue'];
 
-        return $average;
+        // RBG to Luma --- Digital ITU BT.601
+        return 0.33 * $colors['red'] + 0.5 * $colors['green'] + 0.16 * $colors['blue'];
     }
 }
