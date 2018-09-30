ff51ae1f604a33be9c9ba808c2080e529282a3c0
Jim Lind <spoon.vw@gmail.com>
2018-09-30 16:27:39 -0400

    This is turning into a hot mess... commit it
    Just so I don't miss anything... no longer doing anything slope related so the file isn't named properly.

--- lib/Locators/TopSlopeLocator.php
+++ lib/Locators/TopSlopeLocator.php
@@ -13,24 +13,69 @@ class TopSlopeLocator extends SlopeLocator{
         $b = $this->x($this->dataModel->getCenterColumn());
         $c = $this->x($this->dataModel->getRightColumn());
 
-        return MathHelper::average([$a, $b, $c]);
+        $d = array_filter([$a, $b, $c]);
+        //print_r([$a,$b,$c]);
+
+        return MathHelper::median($d);
     }
 
     public function x($list) {
-        $listSlice = array_slice($list, $this->startingPosition, 80, true);
+        $pa = array_slice($list, $this->startingPosition, 10);
+        $pv = MathHelper::average($pa) + 3;
 
+        $s = array_slice($list, $this->startingPosition, 80, true);
+        foreach($s as $k => $v) {
+            if ($v > $pv) {
+                return $k;
+            }
+        }
+        return 0;
+    }
+
+    public function z($list) {
+        $slopeList = [];
+        for($a = 0; $a <= 80; $a += 10) {
+            $p = $this->startingPosition + $a;
+            $j = MathHelper::average(array_slice($list, $p - 1, 3));
+            $k = MathHelper::average(array_slice($list, $p + 9, 3));
+            $slopeList[$p] = ($k - $j);
+        }
+        print_r($slopeList);
+        $index = array_search(max($slopeList), $slopeList);
+        $slopeList = [];
+        for($a = -2; $a <= 12; $a++) {
+            $p = $index + $a;
+            $j = MathHelper::average(array_slice($list, $p - 1, 3));
+            $k = MathHelper::average(array_slice($list, $p + 9, 3));
+            $slopeList[$p] = ($k - $j);
+        }
+        $index = array_search(max($slopeList), $slopeList);
+
+        return $index;
+    }
+
+    public function y($list) {
+        $listSlice = array_slice($list, $this->startingPosition, 80, true);
+        print_r($listSlice);
         $prevValue = 0;
         $slopeList = [];
+        $slopeListX = [];
         foreach ($listSlice as $key => $value) {
             if ($key % 10 !== 0) {
                 continue;
             }
             if ($prevValue) {
+                $valueX = MathHelper::average(array_slice($listSlice, $key-1, 3));
+                $prevValueX = MathHelper::average(array_slice($listSlice, $key-11, 3));
+                $slopeListX[$key - 10] = $valueX - $prevValueX;
+
                 $slopeList[$key - 10] = $value - $prevValue;
             }
             $prevValue = $value;
         }
-        $index = array_search(min($slopeList), $slopeList);
+        print_r($slopeListX);
+        print_r($slopeList);
+        $index = array_search(max($slopeList), $slopeList);
 
         $prevValue = $listSlice[$index];
         $slopeList = [];
@@ -40,7 +85,8 @@ class TopSlopeLocator extends SlopeLocator{
             $slopeList[$nextIndex] = $nextValue - $prevValue;
             $prevValue = $nextValue;
         }
-        $index = array_search(min($slopeList), $slopeList);
+        print_r($slopeList);
+        $index = array_search(max($slopeList), $slopeList);
 
         return $index;
     }
